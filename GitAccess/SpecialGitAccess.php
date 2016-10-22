606bb9d34bfbff071b3645cf10f9ab3ab902d8c1
MTres19 <matthewtrescott@gmail.com>
2016-10-22 15:25:09 -0400

    Refactoring---move authentication to communication file

--- GitAccess/SpecialGitAccess.php
+++ GitAccess/SpecialGitAccess.php
@@ -64,8 +64,9 @@ class SpecialGitAccess extends SpecialPage
     public function executeGitService($subpath, $request_service)
     {
         $this->output->disable(); // Take over output
+        $communication = new GitClientCommunication($this->request, $this->response, $subpath);
         
-        if ($this->auth() == true) // Verify user, don't do anything on failure (auth() handles that)
+        if ($communication->auth() == true) // Verify user, don't do anything on failure (auth() handles that)
         {
             // Put subpath into an array for easy access
             $token = strtok($subpath, "/");
@@ -101,62 +102,7 @@ class SpecialGitAccess extends SpecialPage
         return true; // Overload class to show that this may perform database writes
     }
     
-    protected function auth()
-    {
-        // Auth
-        $authManagerSingleton = MediaWiki\Auth\AuthManager::singleton();
-        $user = User::newFromName($_SERVER["PHP_AUTH_USER"]);
-        
-        if (empty($user) || empty($_SERVER["PHP_AUTH_PW"])) /* No username sent */
-        {
-            $this->response->header('WWW-Authenticate: Basic realm="MediaWiki"');
-            $this->response->header("HTTP/1.1 401 Unauthorized");
-        }
-        else
-        {
-            /* Create AuthenticationRequest---add username and password to object using loadRequestsFromSubmission()
-             * 
-             * The User parameter is needed because there is no session data provided by the git client.
-             */
-            $authenticationRequest = MediaWiki\Auth\AuthenticationRequest::loadRequestsFromSubmission(
-                $authManagerSingleton->getAuthenticationRequests($authManagerSingleton::ACTION_LOGIN, $user),
-                [
-                    "username" => $_SERVER["PHP_AUTH_USER"],
-                    "password" => $_SERVER["PHP_AUTH_PW"],
-                ]
-            );
-            
-            // Check password
-            $authResult = $authManagerSingleton->beginAuthentication($authenticationRequest, ':null');
-            
-            if ($authResult->status == $authResult::PASS && $user->isAllowed("gitaccess"))
-            {
-                return true;
-            }
-            elseif ($authResult->status == $authResult::PASS && !$user->isAllowed("gitaccess"))
-            {
-                $this->response->header("HTTP/1.1 403 Forbidden");
-                
-                echo "Sorry, accessing this wiki with Git requires permissions which you do not have." . PHP_EOL;
-                return false;
-            }
-            elseif ($authResult->status == $authResult::FAIL)
-            {
-                $this->response->header('WWW-Authenticate: Basic realm="MediaWiki"');
-                $this->response->header("HTTP/1.1 401 Unauthorized");
-                
-                return false;
-            }
-            else
-            {
-                $this->response->header('WWW-Authenticate: Basic realm="MediaWiki"');
-                $this->response->header("HTTP/1.1 501 Not Implemented");
-                
-                echo "Your request cannot be processed as authentication requires further information." . PHP_EOL;
-                return false;
-            }
-        }
-    }
+    
 }
 
 
---
+++ GitAccess/includes/communication_manager.php
@@ -0,0 +1,75 @@
+<?php
+
+class GitClientCommunication
+{
+    protected $requst;
+    protected $response;
+    protected $path;
+    
+    public function __construct(&$request, &$response, $path)
+    {
+        $this->request = $request;
+        $this->response = $response;
+        $this->path = $path;
+    }
+    
+    public function auth()
+    {
+        // Auth
+        $authManagerSingleton = MediaWiki\Auth\AuthManager::singleton();
+        $user = User::newFromName($_SERVER["PHP_AUTH_USER"]);
+        
+        if (empty($user) || empty($_SERVER["PHP_AUTH_PW"])) /* Missing username or password */
+        {
+            $this->response->header('WWW-Authenticate: Basic realm="MediaWiki"');
+            $this->response->header("HTTP/1.1 401 Unauthorized");
+        }
+        else
+        {
+            /* Create AuthenticationRequest---add username and password to object using loadRequestsFromSubmission()
+             * 
+             * The User parameter is needed because there is no session data provided by the git client.
+             */
+            $authenticationRequest = MediaWiki\Auth\AuthenticationRequest::loadRequestsFromSubmission(
+                $authManagerSingleton->getAuthenticationRequests($authManagerSingleton::ACTION_LOGIN, $user),
+                [
+                    "username" => $_SERVER["PHP_AUTH_USER"],
+                    "password" => $_SERVER["PHP_AUTH_PW"],
+                ]
+            );
+            
+            // Check password
+            $authResult = $authManagerSingleton->beginAuthentication($authenticationRequest, ':null');
+            
+            if ($authResult->status == $authResult::PASS && $user->isAllowed("gitaccess"))
+            {
+                return true;
+            }
+            elseif ($authResult->status == $authResult::PASS && !$user->isAllowed("gitaccess"))
+            {
+                $this->response->header("HTTP/1.1 403 Forbidden");
+                
+                echo "Sorry, accessing this wiki with Git requires permissions which you do not have." . PHP_EOL;
+                return false;
+            }
+            elseif ($authResult->status == $authResult::FAIL)
+            {
+                $this->response->header('WWW-Authenticate: Basic realm="MediaWiki"');
+                $this->response->header("HTTP/1.1 401 Unauthorized");
+                
+                return false;
+            }
+            else
+            {
+                $this->response->header('WWW-Authenticate: Basic realm="MediaWiki"');
+                $this->response->header("HTTP/1.1 501 Not Implemented");
+                
+                echo "Your request cannot be processed as authentication requires further information." . PHP_EOL;
+                return false;
+            }
+        }
+    }
+    
+}
+
+?>
