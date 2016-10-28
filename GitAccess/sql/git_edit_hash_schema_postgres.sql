6dc269736a01656ae1964a1cb1a5db02ed4c8273
MTres19 <matthewtrescott@gmail.com>
2016-10-28 19:58:20 -0400

    Updates to schema

--- GitAccess/sql/git_edit_hash_schema_postgres.sql
+++ GitAccess/sql/git_edit_hash_schema_postgres.sql
@@ -10,8 +10,13 @@
 
 
 CREATE TABLE IF NOT EXISTS /*_*/git_edit_hash(
+    -- The commit connected to this edit, in case of multiple
+    -- modifications per commit
     commit_hash VARCHAR(40) NOT NULL FOREIGN KEY REFERENCES git_hash(commit_hash),
-    affected_rev_id INTEGER,
-    affected_page_id INTEGER
+    
+    -- The revision ID referenced by the commit. This can't be a foreign
+    -- key as that would prevent pages being moved to the archive table.
+    -- The page ID can be found from the revision table.
+    affected_rev_id INTEGER
 ) /*$wgDBTableOptions*/;
     
--- GitAccess/sql/git_status_modify_hash_schema_mysql.sql
+++ GitAccess/sql/git_status_modify_hash_schema_mysql.sql
@@ -1,17 +1,26 @@
 -- git_status_modify_hash table
 -- 
--- Used to log changes to page status (i.e. moving and deleting).
+-- Used to log changes to page status (i.e. moving and deleting) as
+-- well as file uploads and deletions.
 -- 
 -- These aren't stored as edits and therefore can't be automatically
--- generated on-the-fly. These actions can't be generated from the
+-- generated on-the-fly. (Note that pages in the File namespace are
+-- in fact regular pages.) These actions can't be generated from the
 -- logging table because local commits could include multiple changes
 -- and there is no way to mark these changes as being part of the same
 -- commit. Also, Git stores a committer in addition to an author for
 -- a commit. The same problems hold true for edits.
 
 CREATE TABLE IF NOT EXISTS /*_*/git_status_modify_hash(
+    -- Foreign key to commit_hash in the git_hash table. This assigns an action
+    -- like a page move to a commit, in case it is part of a single commit that
+    -- includes other modifications.
     commit_hash VARCHAR(40) NOT NULL,
+    
+    -- Foreign key to log_id in MediaWiki's logging table. From this key GitAccess
+    -- can infer all the information it needs about the change itself.
     log_id INTEGER NOT NULL,
+    
     FOREIGN KEY (commit_hash) REFERENCES git_hash(commit_hash),
     FOREIGN KEY (log_id) REFERENCES logging(log_id)
 )/*$wgDBTableOptions*/;
